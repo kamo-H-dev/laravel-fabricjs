094fe40b820e12e0a580bf30cf372e75eaa3d94c
Kamo Harutyunyan <kamoharutyunyan.dev@gmail.com>
2019-11-19 18:50:48 +0400

    ui improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,40 +25,41 @@
             @endif
 
             <div class="content">
-
                 <div class="container">
-                    <div class="row">
+                    <div class="row d-flex flex-row top-sect">
                         <div class="title m-b-md">
                             <h3>Laravel & Fabric Js</h3>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-9">
-                            <canvas id="canvas" width="800" height="450"></canvas>
-                        </div>
-                        <div class="col-3">
-                            <button id="reset-elements" class="btn btn-danger">Reset</button>
-                            <button id="save-elements" class="btn btn-primary">Save Elements</button>
+                    <div class="row d-flex flex-row">
+                        <div class="col-3 left-sidebar">
+                            <div class="d-flex justify-content-around">
+                                <button id="reset-elements" class="btn btn-danger">Reset</button>
+                                <button id="save-elements" class="btn btn-primary">Save Elements</button>
+                            </div>
                             <ul id="show-elements" class="list-group"></ul>
                         </div>
-
-                    </div>
-                    <div class="row btn-actions">
-                        <div class="col-md">
-                            <button id="rectangle" data-hint="Rect" class="btn btn-info">+ Rectangle</button>
-                        </div>
-                        <div class="col-md">
-                            <button id="circle" data-hint="Circle" class="btn btn-info">+ Circle</button>
-                        </div>
-                        <div class="col-md">
-                            <button id="triangle" data-hint="Polygon" class="btn btn-info">+ Triangle</button>
-                        </div>
-                        <div class="col-md">
-                            <button id="remove" disabled class="btn btn-danger">Remove Element</button>
+                        <div class="title m-b-md">
+                            <div class="row btn-actions">
+                                <div class="col-md">
+                                    <button id="rectangle" data-hint="Rect" class="btn btn-primary">+ Rectangle</button>
+                                </div>
+                                <div class="col-md">
+                                    <button id="circle" data-hint="Circle" class="btn btn-primary">+ Circle</button>
+                                </div>
+                                <div class="col-md">
+                                    <button id="triangle" data-hint="Polygon" class="btn btn-primary">+ Triangle</button>
+                                </div>
+                                <div class="col-md">
+                                    <button id="remove" disabled class="btn btn-danger">Remove Element</button>
+                                </div>
+                            </div>
+                            <div class="col-8">
+                                <canvas id="canvas" width="800" height="450"></canvas>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
         <script src="{{ mix('js/app.js') }}"></script>
